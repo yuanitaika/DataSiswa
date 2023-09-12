7a56eb1848bfa8d4fe988eb388a62aa2f128b6fd
yuanitaika <144770603+yuanitaika@users.noreply.github.com>
2023-09-12 15:20:23 +0700

    Add files via upload

---
+++ uiyuan/index.php
@@ -0,0 +1,51 @@
+<?php include('config.php');?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Siswa</title>
+</head>
+<body>
+    <header><h3>Daftar Siswa</h3></header>
+    <nav><a href="v_form.php"><button>Tambah Baru</button></a></nav>
+    <br>
+    <table border ="1" cellpadding = "8">
+    <thread>
+    <tr>
+    <th>NIS</th>
+    <th>Nama</th>
+    <th>Alamat</th>
+    <th>Jenis Kelamin</th>
+    <th>No Telepon</th>
+    <th>Kelas</th>
+    <th>Tindakan</th>
+    </tr>
+    </thread>
+<tbody>
+<?php
+$sql = "SELECT * FROM t_siswa";
+$query = mysqli_query($db, $sql);
+while($siswa = mysqli_fetch_array($query)){
+    echo "<tr>";
+    echo "<td>".$siswa['nis']."</td>";
+    echo "<td>".$siswa['nama']."</td>";
+    echo "<td>".$siswa['alamat']."</td>";
+    echo "<td>".$siswa['jenis_kelamin']."</td>";
+    echo "<td>".$siswa['no_telp']."</td>";
+    echo "<td>".$siswa['kelas']."</td>";
+    echo "<td>";
+    echo "<a href= 'v_edit.php?nis=".$siswa['nis']."'>Edit</a> | ";
+    echo "<a href= 'hapus.php?nis=".$siswa['nis']."'>Hapus</a>";
+    echo "</td>";
+    echo "</tr>";
+}
+?>
+</tbody>
+</table>
+<p>Total:
+<?php echo mysqli_num_rows($query) ?>
+</p>
+</body>
+</html>
---
+++ uiyuan/proses_pendaftaran.php
@@ -0,0 +1,31 @@
+<?php
+include("config.php");
+//cek apakah tombol simpan sudah diklik atau belum
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+    //ambil data dari form
+    $nis = $_POST['nis'];
+    $nama = $_POST['nama'];
+    $alamat = $_POST['alamat'];
+    $jk = $_POST['jenis_kelamin'];
+    $no_telp = $_POST['no_telp'];
+    $kelas = $_POST['kelas'];
+
+    //buat query
+    $sql = "INSERT INTO t_siswa(nis, nama, alamat, jenis_kelamin, no_telp, kelas)
+    VALUE('$nis','$nama','$alamat','$jk','$no_telp','$kelas')";
+    $query = mysqli_query($db, $sql) or die(mysqli_error($db));
+
+    //apakah query update berhasil
+    if($query){
+
+        //kalau berhasil, alihkan ke halaman index.php dengan status=sukses
+        header('location:index.php?status=sukses');
+    }else{
+
+        //kalau gagal, alihkan ke halaman index.php dengan status = gagal
+        header('location:index.php?status=gagal');
+    }
+    }else{
+        die("Akses dilarang ...");
+    }
+    ?>
